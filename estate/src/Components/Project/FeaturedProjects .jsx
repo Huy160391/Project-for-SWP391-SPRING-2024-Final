4c9044061d84f5c87dbde23364ff8fbb45e7f072
yyenthaonek <142802011+yyenthaonek@users.noreply.github.com>
2024-03-25 16:47:53 +0700

    UpdateFeaturedProjects

--- estate/src/Components/Project/FeaturedProjects .jsx
+++ estate/src/Components/Project/FeaturedProjects .jsx
@@ -5,6 +5,7 @@ import ProjectCard from './ProjectCard'; // Make sure the name matches the actua
 const FeaturedProjects = () => {
   const [projects, setProjects] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -21,6 +22,14 @@ const FeaturedProjects = () => {
     fetchProjects();
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProjects = projects.filter(project => {
+    return project.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   if (isLoading) {
     return <div>Loading...</div>; // Or any other loading indicator
   }
@@ -29,11 +38,39 @@ const FeaturedProjects = () => {
     <section className="py-12 bg-white">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-6">Featured Projects</h2>
-        {/* Adjusted the grid to lg:grid-cols-4 for 4 cards in a row on large screens */}
+        <div className="flex justify-center mb-10">
+          <div className="relative w-3/4">
+            <input
+              type="text"
+              placeholder="Search projects..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="border border-gray-300 px-3 py-2 rounded-lg w-full pr-10 focus:outline-none"
+            />
+            <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+              <svg
+                className="w-5 h-5 text-black"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M21 21l-5.2-5.2M11 15c-3.3 0-6-2.7-6-6s2.7-6 6-6 6 2.7 6 6-2.7 6-6 6z"></path>
+              </svg>
+            </span>
+          </div>
+        </div>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-          {projects.map((project) => (
-            <ProjectCard key={project.projectId} project={project} /> // Make sure your key is unique and the props match your ProjectCard component's expected props
-          ))}
+          {filteredProjects.length > 0 ? (
+            filteredProjects.map((project) => (
+              <ProjectCard key={project.projectId} project={project} />
+            ))
+          ) : (
+            <div>No projects found.</div>
+          )}
         </div>
       </div>
     </section>
