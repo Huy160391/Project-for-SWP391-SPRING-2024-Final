134e544428063421d249e862d33f180735d8ee00
Huy160391 <101168278+Huy160391@users.noreply.github.com>
2024-03-27 00:41:31 +0700

    updateMail

--- estate/src/Components/DashboardAgency/EditApartmentPage.jsx
+++ estate/src/Components/DashboardAgency/EditApartmentPage.jsx
@@ -34,26 +34,20 @@ const EditApartmentPage = () => {
 
     //Check valid for description: at least 10 characters and not exceed 1000 characters
     useEffect(() => {
-<<<<<<< HEAD
+
         if (apartment.description) {
-            if (apartment.description.length < 10) {
+
+            if (apartment.description && apartment.description.length < 10) {
                 setDescriptionError('Description must be at least 10 characters long.');
-            } else if (apartment.description.length > 1000) {
+            } else if (apartment.description && apartment.description.length > 1000) {
                 setDescriptionError('Description cannot exceed 1000 characters.');
             } else {
                 setDescriptionError('');
+
             }
-=======
-        if (apartment.description && apartment.description.length < 10) {
-            setDescriptionError('Description must be at least 10 characters long.');
-        } else if (apartment.description && apartment.description.length > 1000) {
-            setDescriptionError('Description cannot exceed 1000 characters.');
-        } else {
-            setDescriptionError('');
->>>>>>> 6cb0f46d7ed58604bf5ab04476d28fbde189ef29
         }
     }, [apartment.description]);
-    
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         setApartment(prevState => ({ ...prevState, [name]: value }));
