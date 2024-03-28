8249e716d9b9eda346c35d28682b4edf546d5983
Huy160391 <101168278+Huy160391@users.noreply.github.com>
2024-03-28 22:38:21 +0700

    update

--- estate/src/Components/Login/Login.jsx
+++ estate/src/Components/Login/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     navigate(path);
     window.location.reload();
   };
-  
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -72,44 +72,43 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 font-serif">
-    <div className="w-full max-w-md px-8 py-6 bg-white rounded-lg shadow-md">
-      <h1 className="mb-4 text-2xl font-bold text-center text-gray-900 py-10">LOG IN</h1>
-      <div className="mb-4 text-sm text-center text-gray-600"></div>
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <div className="form-group">
-          <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter your username"
-            className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:ring-blue-500 focus:border-blue-500"
-          />
+      <div className="w-full max-w-md px-8 py-6 bg-white rounded-lg shadow-md">
+        <h1 className="mb-4 text-2xl font-bold text-center text-gray-900 py-10">LOG IN</h1>
+        <div className="mb-4 text-sm text-center text-gray-600"></div>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="form-group">
+            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter your username"
+              className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <button type="submit" className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+            LOG IN
+          </button>
+          {loginError && <div className="text-sm text-red-600">{loginError}</div>}
+        </form>
+        <div className="mt-6 text-sm text-center text-gray-600">
+          <span className="mx-2">|</span>
+          <span>No account? <a href="/registration" className="text-blue-600 hover:text-blue-700">Register</a></span>
         </div>
-        <div className="form-group">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-            className="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:ring-blue-500 focus:border-blue-500"
-          />
-        </div>
-        <button type="submit" className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-        LOG IN
-        </button>
-        {loginError && <div className="text-sm text-red-600">{loginError}</div>}
-      </form>
-      <div className="mt-6 text-sm text-center text-gray-600">
-        <a href="/forgot-password" className="text-blue-600 hover:text-blue-700">Forgot password?</a>
-        <span className="mx-2">|</span>
-        <span>No account? <a href="/registration" className="text-blue-600 hover:text-blue-700">Register</a></span>
       </div>
     </div>
-  </div>
   );
 };
 
